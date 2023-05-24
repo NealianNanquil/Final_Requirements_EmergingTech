909e9f775ca66e3c86009cb50f9681ba9f4ea94e
Jay Alfonso <111827889+jayalfonso13@users.noreply.github.com>
2023-05-24 21:50:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ st.title("Shoe Classifier")
 file=st.file_uploader("Choose a photo from your computer",type=["jpg","png"])
 
 def import_and_predict(image_data,model):
-    size=(128,128)
+    size=(256,256)
     image = ImageOps.fit(image_data,size, Image.LANCZOS)
     image = np.asarray(image)
     image = image / 255.0
-    img_reshape = np.reshape(image, (1, 128, 128, 3))
+    img_reshape = np.reshape(image, (1, 256, 256, 3))
     prediction = model.predict(img_reshape)
     return prediction
 if file is None:
