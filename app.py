82d2db5472af83cdc5e33ebbad177709c55c4bdc
Jay Alfonso <111827889+jayalfonso13@users.noreply.github.com>
2023-05-24 21:48:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['Apple', 'Tomato']
+    class_names=['Boots', 'Sandals', 'Slippers']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
